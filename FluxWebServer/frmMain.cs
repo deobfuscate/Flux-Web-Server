3ca3beff26fcc654d64f9aff6433746ff91ad45c
Dan <33081566+deobfuscate@users.noreply.github.com>
2022-09-20 23:30:16 -0700

    Remove Program.cs

--- FluxWebServer/frmMain.cs
+++ FluxWebServer/frmMain.cs
@@ -97,4 +97,17 @@ namespace FluxWebServer
             Log(e.Message);
         }
     }
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
 }
