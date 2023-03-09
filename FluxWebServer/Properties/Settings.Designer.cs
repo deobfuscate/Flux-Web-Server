7e0ab1c7946e11749b47ac0525487f9ef9fd9aa7
Dan <33081566+deobfuscate@users.noreply.github.com>
2023-03-08 23:21:11 -0800

    phpEnabled setting

--- FluxWebServer/Properties/Settings.Designer.cs
+++ FluxWebServer/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace FluxWebServer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -56,6 +56,18 @@ namespace FluxWebServer.Properties {
             }
             set {
                 this["phpPath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool phpEnabled {
+            get {
+                return ((bool)(this["phpEnabled"]));
+            }
+            set {
+                this["phpEnabled"] = value;
             }
         }
     }
